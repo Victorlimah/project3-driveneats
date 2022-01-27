ba461bbe9ad9b9230c2931e5a5ba834da5dcd022
Victor Gabriel de Lima <victorgabriel.lima03@gmail.com>
2022-01-26 23:44:03 -0300

    criando anotacoes no js

--- js/script.js
+++ js/script.js
@@ -1,2 +1,11 @@
 // QUANDO SELECIONAR ITEM FAZER ISSO:
 // inset 0px 0px 0px 5px #32b72f
+
+// quando clickar em um os outros ficam sem borda
+
+// botão quando os 3 não estiverem marcados:
+// Selecione os 3 itens para fechar o pedido
+
+// quando tiver marcado:
+// Fechar pedido
+// background: #32B72F;
