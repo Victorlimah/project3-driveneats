a6d6f93e8a4f8723e1ed9891309ec936122b8627
Victor Gabriel de Lima <victorgabriel.lima03@gmail.com>
2022-01-27 00:21:36 -0300

    somente uma escolha por comida

--- js/script.js
+++ js/script.js
@@ -14,3 +14,40 @@ setTimeout(function () {
   let paginaCarregando = document.getElementById("inicio");
   paginaCarregando.style.display = "none";
 }, 1500);
+
+function marcarMiojoEscolha() {
+  zerarEscolhasComidas();
+  let itemSelecionado = document.getElementById("miojo");
+  itemSelecionado.style.boxShadow =
+    "0px 0px 10px -4px rgba(0, 0, 0, 0.25), inset 0px 0px 0px 5px #32B72F";
+}
+
+function marcarMacarraoEscolha() {
+  zerarEscolhasComidas();
+  let itemSelecionado = document.getElementById("macarronada");
+  itemSelecionado.style.boxShadow =
+    "0px 0px 10px -4px rgba(0, 0, 0, 0.25), inset 0px 0px 0px 5px #32B72F";
+}
+
+function marcarArrozEscolha() {
+  zerarEscolhasComidas();
+  let itemSelecionado = document.getElementById("arroz");
+  itemSelecionado.style.boxShadow =
+    "0px 0px 10px -4px rgba(0, 0, 0, 0.25), inset 0px 0px 0px 5px #32B72F";
+}
+
+function marcarFeijoadaEscolha() {
+  zerarEscolhasComidas();
+  let itemSelecionado = document.getElementById("feijoada");
+  itemSelecionado.style.boxShadow =
+    "0px 0px 10px -4px rgba(0, 0, 0, 0.25), inset 0px 0px 0px 5px #32B72F";
+}
+
+function zerarEscolhasComidas() {
+  let itemMiojo = document.getElementById("miojo");
+  let itemMacarronada = document.getElementById("macarronada");
+  let itemArroz = document.getElementById("arroz");
+  let itemFeijoada = document.getElementById("feijoada");
+  itemMiojo.style.boxShadow = "0px 0px 10px -4px rgba(0, 0, 0, 0.25)";
+  itemMacarronada.style.boxShadow = "0px 0px 10px -4px rgba(0, 0, 0, 0.25)";
+}
