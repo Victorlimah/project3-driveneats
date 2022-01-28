192c7160328769c468143138dc2a16dc7a4e2fbf
Victor Gabriel de Lima <victorgabriel.lima03@gmail.com>
2022-01-27 22:06:08 -0300

    sistema de selecao de produto refeito sem bugs

--- js/script.js
+++ js/script.js
@@ -19,19 +19,51 @@ const primeiroItem = document.querySelector(".primeiroItem");
 const segundoItem = document.querySelector(".segundoItem");
 const terceiroItem = document.querySelector(".terceiroItem");
 const quartoItem = document.querySelector(".quartoItem");
+const comidas = ["miojo", "macarrao", "arroz", "feijoada"];
+const bebidas = ["coca", "pepsi", "fanta", "agua"];
+const sobremesas = ["pudim", "petit", "sorvete", "acai"];
+const listaOpcoes = [primeiroItem, segundoItem, terceiroItem, quartoItem];
 
-function marcarPrimeiraEscolha() {
-  primeiroItem.classList.toggle("background-selecionado");
+function marcarEscolha(itemSelecionado) {
+  zerarEscolhas(itemSelecionado);
+  let nomeItem = document.getElementById(itemSelecionado);
+
+  for (i = 0; i < 4; i++) {
+    if (
+      itemSelecionado === comidas[i] ||
+      itemSelecionado === bebidas[i] ||
+      itemSelecionado === sobremesas[i]
+    ) {
+      nomeItem.classList.toggle("background-selecionado");
+    }
+  }
 }
 
-function marcarSegundaEscolha() {
-  segundoItem.classList.toggle("background-selecionado");
-}
+function zerarEscolhas(itemCardapio) {
+  let comida;
+  let bebida;
+  let sobremesa;
 
-function marcarTerceiraEscolha() {
-  terceiroItem.classList.toggle("background-selecionado");
-}
-
-function marcarQuartaEscolha() {
-  quartoItem.classList.toggle("background-selecionado");
+  if (comidas.includes(itemCardapio)) {
+    for (i = 0; i < 4; i++) {
+      comida = document.getElementById(comidas[i]);
+      if (comida.classList.contains("background-selecionado")) {
+        comida.classList.remove("background-selecionado");
+      }
+    }
+  } else if (bebidas.includes(itemCardapio)) {
+    for (i = 0; i < 4; i++) {
+      bebida = document.getElementById(bebidas[i]);
+      if (bebida.classList.contains("background-selecionado")) {
+        bebida.classList.remove("background-selecionado");
+      }
+    }
+  } else if (sobremesas.includes(itemCardapio)) {
+    for (i = 0; i < 4; i++) {
+      sobremesa = document.getElementById(sobremesas[i]);
+      if (sobremesa.classList.contains("background-selecionado")) {
+        sobremesa.classList.remove("background-selecionado");
+      }
+    }
+  }
 }
